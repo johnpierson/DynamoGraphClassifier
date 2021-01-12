39ed3e59447ec37190c58d35ee0c068e23f904dc
john pierson <sixtysecondrevit@gmail.com>
2021-01-12 15:50:32 -0700

    Initial upload

---
+++ src/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using lobe;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using lobe.ImageSharp;
+using Microsoft.Extensions.Configuration;
+
+namespace ClassifyDynamoGraph
+{
+    class Program
+    {
+        private static readonly IConfiguration Configuration =
+            new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .Build();
+
+        static void Main(string[] args)
+        {
+            var signatureFilePath = Configuration["sig_file_path"];
+            var imageToClassify = Configuration["image_to_classify"];
+
+            ImageClassifier.Register("onnx", () => new OnnxImageClassifier());
+            using var classifier = ImageClassifier.CreateFromSignatureFile(
+                new FileInfo(signatureFilePath));
+
+            var results = classifier.Classify(Image
+                .Load(imageToClassify).CloneAs<Rgb24>());
+            Console.WriteLine(results.Classifications.First().Label);
+        }
+    }
+}
