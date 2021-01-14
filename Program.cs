f91bc744399056eddd4b998004d1f79f4a4d93d3
john pierson <sixtysecondrevit@gmail.com>
2021-01-14 13:22:15 -0700

    Fixes for it to classify tweets

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using lobe.ImageSharp;
 using Microsoft.Extensions.Configuration;
+using TweetSharp;
 
 namespace ClassifyDynamoGraph
 {
@@ -20,13 +21,73 @@ namespace ClassifyDynamoGraph
 
         static void Main(string[] args)
         {
-            var signatureFilePath = Configuration["sig_file_path"];
-            var imageToClassify = Configuration["image_to_classify"];
+            
+            string status = string.Empty;
 
+            var service = new TwitterService(
+                Configuration["twitter_consumer_key"],
+                Configuration["twitter_consumer_secret"]
+            );
+
+            service.AuthenticateWith(
+                Configuration["twitter_access_token"],
+                Configuration["twitter_access_token_secret"]
+            );
+
+            var mentionOptons = new ListTweetsMentioningMeOptions();
+
+            var newMentions = service.ListTweetsMentioningMe(mentionOptons).ToList();
+
+            if (!newMentions.Any()) return;
+
+
+            foreach (var mention in newMentions)
+            {
+                if (mention.IsFavorited)
+                {
+                    continue;
+                }
+
+                if (mention.Entities.Media.Any())
+                {
+                    status = "Hey! There is no Dynamo graph image in this tweet. Much sad.";
+                }
+
+                string classificationImage = ClassifyImage(mention.Entities.Media.First().MediaUrl);
+
+                switch (classificationImage)
+                {
+                    case "Yes":
+                        status = "nice job this graph has annotations";
+                        break;
+                    case "No":
+                        status = "this graph has no annotations and makes me sad.";
+                        break;
+                }
+
+                //favorite the tweet so we don't do it again
+                FavoriteTweetOptions favoriteOptions = new FavoriteTweetOptions { Id = mention.Id };
+                service.FavoriteTweet(favoriteOptions);
+
+                var result = service.SendTweet(new SendTweetOptions
+                {
+                    Status = status,
+                    InReplyToStatusId = mention.Id,
+                    AutoPopulateReplyMetadata = true
+                });
+            }
+
+        }
+
+
+
+        private static string ClassifyImage(string imageUrl)
+        {
+            var signatureFilePath = Configuration["sig_file_path"]; ;
 
             using (WebClient client = new WebClient())
             {
-                client.DownloadFile(new Uri(imageToClassify), @"c:\temp\image.png");
+                client.DownloadFile(new Uri(imageUrl), @"c:\temp\image.png");
             }
 
             ImageClassifier.Register("onnx", () => new OnnxImageClassifier());
@@ -36,6 +97,8 @@ namespace ClassifyDynamoGraph
             var results = classifier.Classify(Image
                 .Load(@"c:\temp\image.png").CloneAs<Rgb24>());
             Console.WriteLine(results.Classifications.First().Label);
+
+            return results.Classifications.First().Label;
         }
     }
 }
